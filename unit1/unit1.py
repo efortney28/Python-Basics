460bba6225a321c70887c81e2f209b8fab15f749
Eric Fortney <efortney@Erics-MBP.lan>
2022-01-19 12:36:59 -0800

    initial commit

---
+++ unit1/unit1.py
@@ -0,0 +1,28 @@
+'''
+    Syntax, Comments, Variables, Data Types, & Numbers
+
+
+    Below you will find a partially complete program. Don't worry about not understanding what is going on after the break!
+
+    Just fill in the blanks which will be marked with line comments like this: #This is a comment
+'''
+
+### Create 2 variables: one named name, and one named favoriteColor
+### these variables are case-sensitive so be sure to double check spelling and case
+### Set name equal to your name or somebody else's
+### Set favoriteColor equal to any color you'd like
+
+
+### add your variables here
+
+
+
+
+
+'''
+Do not worry about what the following line(s) do just yet
+'''
+
+print("My name is {0} and my favorite color is {1}.".format(name, favoriteColor))
+
+''' This should display to your terminal something like "My name is Eric and my favorite color is blue." '''
